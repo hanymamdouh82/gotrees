b0a9e19705d92d32ca6a5adde9f11ec681a4421c
Hany Mamdouh <hany@atentec.com>
2023-09-01 22:53:21 +0300

    added copyrights to files

--- test_dataset.go
+++ test_dataset.go
@@ -1,3 +1,6 @@
+// Copyright 2023 Hany Mamdouh. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
 package gotrees
 
 type Person struct {
